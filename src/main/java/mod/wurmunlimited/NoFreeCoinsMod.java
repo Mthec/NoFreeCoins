5ba1998f59a5ff0041ed6d833e959bd0cdc883fd
Daniel <daniel4592@hotmail.com>
2021-10-11 16:33:00 +0100

    Added allow_sell_at_token option to .properties.

--- src/main/java/mod/wurmunlimited/NoFreeCoinsMod.java
+++ src/main/java/mod/wurmunlimited/NoFreeCoinsMod.java
@@ -1,9 +1,26 @@
 package mod.wurmunlimited;
 
+import com.wurmonline.server.creatures.Creature;
 import org.gotti.wurmunlimited.modloader.classhooks.HookManager;
-import org.gotti.wurmunlimited.modloader.interfaces.*;
+import org.gotti.wurmunlimited.modloader.interfaces.Configurable;
+import org.gotti.wurmunlimited.modloader.interfaces.Initable;
+import org.gotti.wurmunlimited.modloader.interfaces.WurmServerMod;
 
-public class NoFreeCoinsMod implements WurmServerMod, Initable {
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Properties;
+
+public class NoFreeCoinsMod implements WurmServerMod, Configurable, Initable {
+    private boolean sellBlocked = false;
+
+    @Override
+    public void configure(Properties properties) {
+        String val = properties.getProperty("allow_sell_at_token");
+        if (val != null && !val.isEmpty()) {
+            sellBlocked = !val.equals("true");
+        }
+    }
+
     @Override
     public void init() {
         HookManager manager = HookManager.getInstance();
@@ -13,5 +30,20 @@ public class NoFreeCoinsMod implements WurmServerMod, Initable {
                 "checkCoinAward",
                 "(I)Z",
                 () -> (proxy, method, args) -> false);
+
+        manager.registerHook("com.wurmonline.server.behaviours.Methods",
+                "discardSellItem",
+                "(Lcom/wurmonline/server/creatures/Creature;Lcom/wurmonline/server/behaviours/Action;Lcom/wurmonline/server/items/Item;F)Z",
+                () -> this::discardSellItem);
+    }
+
+    Object discardSellItem(Object o, Method method, Object[] args) throws InvocationTargetException, IllegalAccessException {
+        if (sellBlocked) {
+            Creature player = (Creature)args[0];
+            player.getCommunicator().sendNormalServerMessage("You cannot sell items at the token on this server.");
+            return true;
+        } else {
+            return method.invoke(o, args);
+        }
     }
 }
